80e44aff7d999ae970bf5604e8b9eccb883c6604
sharpninja <ninja@thesharp.ninja>
2021-02-17 13:29:16 -0600

    Script

--- gothere.sh
+++ gothere.sh
@@ -5,8 +5,8 @@ echo "::debug::Start."
 main() {
     echo "::debug::Main." # see https://github.com/actions/toolkit/issues/168
 
-    if usesBoolean "${ACTIONS_STEP_DEBUG}"; then
-        echo "::debug::ACTIONS_STEP_DEBUG: $ACTIONS_STEP_DEBUG"
+    if usesBoolean "$(ACTIONS_STEP_DEBUG)"; then
+        echo "::debug::ACTIONS_STEP_DEBUG: $(ACTIONS_STEP_DEBUG)"
         #     echo "::add-mask::${INPUT_USERNAME}"
         #     echo "::add-mask::${INPUT_PASSWORD}"
         set -x
