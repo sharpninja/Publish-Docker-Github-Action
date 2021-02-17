40269525ff90480d0f0608af0e3a3d2e8ad9a4be
The Sharp Ninja <ninja@thesharp.ninja>
2021-02-17 11:02:08 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,15 @@ main() {
   echo "" # see https://github.com/actions/toolkit/issues/168
 
   if usesBoolean "${ACTIONS_STEP_DEBUG}"; then
-    echo "::add-mask::${INPUT_USERNAME}"
-    echo "::add-mask::${INPUT_PASSWORD}"
+#     echo "::add-mask::${INPUT_USERNAME}"
+#     echo "::add-mask::${INPUT_PASSWORD}"
     set -x
   fi
 
   sanitize "${INPUT_NAME}" "name"
   if ! usesBoolean "${INPUT_NO_PUSH}"; then
-    sanitize "${INPUT_USERNAME}" "username"
-    sanitize "${INPUT_PASSWORD}" "password"
+#     sanitize "${INPUT_USERNAME}" "username"
+#     sanitize "${INPUT_PASSWORD}" "password"
   fi
 
   registryToLower
@@ -22,10 +22,12 @@ main() {
   REGISTRY_NO_PROTOCOL=$(echo "${INPUT_REGISTRY}" | sed -e 's/^https:\/\///g')
   if uses "${INPUT_REGISTRY}" && ! isPartOfTheName "${REGISTRY_NO_PROTOCOL}"; then
     INPUT_NAME="${REGISTRY_NO_PROTOCOL}/${INPUT_NAME}"
+    echo "INPUT_NAME: $(INPUT_NAME)" 
   fi
 
   if uses "${INPUT_TAGS}"; then
     TAGS=$(echo "${INPUT_TAGS}" | sed "s/,/ /g")
+    echo "TAGS: $(TAGS)" 
   else
     translateDockerTag
   fi
@@ -35,13 +37,18 @@ main() {
   fi
 
   if uses "${INPUT_USERNAME}" && uses "${INPUT_PASSWORD}"; then
+    echo "docker login -u ${INPUT_USERNAME} -p ${INPUT_PASSWORD} ${INPUT_REGISTRY} --verbose"
     docker login -u ${INPUT_USERNAME} -p ${INPUT_PASSWORD} ${INPUT_REGISTRY} --verbose
   fi
 
   FIRST_TAG=$(echo "${TAGS}" | cut -d ' ' -f1)
+  echo "FIRST_TAG: $(FIRST_TAG)" 
   DOCKERNAME="${INPUT_NAME}:${FIRST_TAG}"
+  echo "DOCKERNAME: $(DOCKERNAME)" 
   BUILDPARAMS=""
+  echo "BUILDPARAMS: $(BUILDPARAMS)" 
   CONTEXT="."
+  echo "CONTEXT: $(CONTEXT)" 
 
   if uses "${INPUT_DOCKERFILE}"; then
     useCustomDockerfile
@@ -51,6 +58,7 @@ main() {
   fi
   if uses "${INPUT_CONTEXT}"; then
     CONTEXT="${INPUT_CONTEXT}"
+    echo "CONTEXT: $(CONTEXT)" 
   fi
   if usesBoolean "${INPUT_CACHE}"; then
     useBuildCache
@@ -86,10 +94,12 @@ sanitize() {
 
 registryToLower(){
  INPUT_REGISTRY=$(echo "${INPUT_REGISTRY}" | tr '[A-Z]' '[a-z]')
+ echo "INPUT_REGISTRY: $(INPUT_REGISTRY)" 
 }
 
 nameToLower(){
   INPUT_NAME=$(echo "${INPUT_NAME}" | tr '[A-Z]' '[a-z]')
+  echo "INPUT_NAME: $(INPUT_NAME)" 
 }
 
 isPartOfTheName() {
@@ -100,23 +110,32 @@ translateDockerTag() {
   local BRANCH=$(echo "${GITHUB_REF}" | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
   if hasCustomTag; then
     TAGS=$(echo "${INPUT_NAME}" | cut -d':' -f2)
+    echo "TAGS: $(TAGS)" 
     INPUT_NAME=$(echo "${INPUT_NAME}" | cut -d':' -f1)
+    echo "INPUT_NAME: $(INPUT_NAME)" 
   elif isOnDefaultBranch; then
     TAGS="latest"
+    echo "TAGS: $(TAGS)" 
   elif isGitTag && usesBoolean "${INPUT_TAG_SEMVER}" && isSemver "${GITHUB_REF}"; then
     if isPreRelease "${GITHUB_REF}"; then
       TAGS=$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g" | sed -E "s/v?([0-9]+)\.([0-9]+)\.([0-9]+)(-[a-zA-Z]+(\.[0-9]+)?)?/\1.\2.\3\4/g")
+      echo "TAGS: $(TAGS)" 
     else
       TAGS=$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g" | sed -E "s/v?([0-9]+)\.([0-9]+)\.([0-9]+)/\1.\2.\3\4 \1.\2\4 \1\4/g")
+      echo "TAGS: $(TAGS)" 
     fi
   elif isGitTag && usesBoolean "${INPUT_TAG_NAMES}"; then
     TAGS=$(echo "${GITHUB_REF}" | sed -e "s/refs\/tags\///g")
+    echo "TAGS: $(TAGS)" 
   elif isGitTag; then
     TAGS="latest"
+    echo "TAGS: $(TAGS)" 
   elif isPullRequest; then
     TAGS="${GITHUB_SHA}"
+    echo "TAGS: $(TAGS)" 
   else
     TAGS="${BRANCH}"
+    echo "TAGS: $(TAGS)" 
   fi;
 }
 
@@ -142,22 +161,26 @@ isPullRequest() {
 
 changeWorkingDirectory() {
   cd "${INPUT_WORKDIR}"
+  echo "INPUT_WORKDIR: $(INPUT_WORKDIR)" 
 }
 
 useCustomDockerfile() {
   BUILDPARAMS="${BUILDPARAMS} -f ${INPUT_DOCKERFILE}"
+  echo "BUILDPARAMS: $(BUILDPARAMS)" 
 }
 
 addBuildArgs() {
   for ARG in $(echo "${INPUT_BUILDARGS}" | tr ',' '\n'); do
     BUILDPARAMS="${BUILDPARAMS} --build-arg ${ARG}"
-    echo "::add-mask::${ARG}"
+    echo "BUILDPARAMS: $(BUILDPARAMS)" 
+    echo "ARG: ${ARG}"
   done
 }
 
 useBuildCache() {
   if docker pull "${DOCKERNAME}" 2>/dev/null; then
     BUILDPARAMS="${BUILDPARAMS} --cache-from ${DOCKERNAME}"
+    echo "BUILDPARAMS: $(BUILDPARAMS)" 
   fi
 }
 
@@ -189,12 +212,15 @@ build() {
   local BUILD_TAGS=""
   for TAG in ${TAGS}; do
     BUILD_TAGS="${BUILD_TAGS}-t ${INPUT_NAME}:${TAG} "
+    echo "BUILD_TAGS: $(BUILD_TAGS)" 
   done
+  echo "docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}"
   docker build ${INPUT_BUILDOPTIONS} ${BUILDPARAMS} ${BUILD_TAGS} ${CONTEXT}
 }
 
 push() {
   for TAG in ${TAGS}; do
+    echo "docker push \"${INPUT_NAME}:${TAG}\""
     docker push "${INPUT_NAME}:${TAG}"
   done
 }
